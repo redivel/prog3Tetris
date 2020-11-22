d455873eb40e3f5dc5f949d23bc973baa724ccf5
redivel <levente2der@gmail.com>
2020-11-23 00:23:33 +0100

    Matrix initialized correctly

--- src/hu/bme/redivel/Tetris/Matrix.java
+++ src/hu/bme/redivel/Tetris/Matrix.java
@@ -4,6 +4,30 @@ public class Matrix {
     private char[][] matrix;
 
     public Matrix() {
-        matrix = new char[1][];
+        matrix = new char[23][12];
+        for (int j = 0; j < 23; j++) {
+            for (int i = 0; i < 12; i++) {
+                if(i == 0 || i == 11 || j == 22){
+                    matrix[j][i] = 'b';
+                }
+                else{
+                    matrix[j][i] = 'e';
+                }
+            }
+        }
+    }
+
+    public void print(){
+        for (int j = 0; j < 23; j++) {
+            for (int i = 0; i < 12; i++) {
+                System.out.print(matrix[j][i]);
+                if(i < 11) {
+                    System.out.print("\t");
+                }
+                else{
+                    System.out.print("\n");
+                }
+            }
+        }
     }
 }
